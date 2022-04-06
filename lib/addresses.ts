8d3228753e8b65138f7a81a1ead97f0e93f5cccd
Chine-Code <chokejiri@gmail.com>
2022-04-06 14:56:58 +0100

    Completed Solana-Pay Tutorial

---
+++ lib/addresses.ts
@@ -0,0 +1,11 @@
+import { PublicKey } from "@solana/web3.js"
+
+// Your shop wallet address
+export const shopAddress = new PublicKey('GkPdSq1LMrJDGHrxdpTntFuhkrjmBm6RyJCSWPGSZg8J')
+
+// usdc-devnet token address
+export const usdcAddress = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr')
+
+// This is your token/coupon address
+export const couponAddress = new PublicKey('A49g151dAuVkVqw8ZrGr12ech6KWfZbzvi6BYngD94vt')
+~            
